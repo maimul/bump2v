736386266e57339684180aa4ba898d162af4ae51
maimul <maimulskates@gmail.com>
2024-01-04 19:48:33 +0600

    Added another condition for checking the prefix for commit

--- bump2v/__main__.py
+++ bump2v/__main__.py
@@ -10,10 +10,7 @@ okay = "Okhay lessgo! 🚀🚀🚀"
 
 def main():
     
-    # Check for uncommitted changes
-    if subprocess.run(["git", "status", "--porcelain"]).stdout:
-        print(f"{holdup} Working directory is not clean. Please commit or discard changes before bumping the version. {thankyou}")
-        exit(1)
+
 
     # Check the latest commit message
     latest_commit_message = subprocess.run(["git", "log", "-1", "--pretty=%B"], capture_output=True, text=True).stdout.strip()
@@ -23,7 +20,12 @@ def main():
     prefix_to_check2 = "Bump version:"
 
     # Check if the latest commit message starts with the specified prefix
-    if latest_commit_message.startswith(prefix_to_check) or latest_commit_message.startswith(prefix_to_check2):
+        # Check for uncommitted changes
+    if subprocess.run(["git", "status", "--porcelain"]).stdout:
+        print(f"{holdup} Working directory is not clean. Please commit or discard changes before bumping the version. {thankyou}")
+        exit(1)
+
+    elif latest_commit_message.startswith(prefix_to_check) or latest_commit_message.startswith(prefix_to_check2):
         print(f"{holdup} Bump version rejected. 😿 There are no changes to the code.")
         exit(1)
     else:
@@ -37,3 +39,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
